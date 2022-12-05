b3eaf993834fa5ac767280eae3cf664fa3e6d92b
Amul Gaurav <amulgaurav003@gmail.com>
2022-12-05 19:42:08 +0530

    Added leap year program

--- LeapYear.java
+++ LeapYear.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 public class LeapYear {
     public static void main(String [] args) {
         Scanner user = new Scanner(System.in);
-        System.out.println("Enter year :");
+        System.out.print("Enter year : ");
         int year = user.nextInt();
         user.close();
         if(year % 4 == 0) {
-            System.out.printf("%d is a Leap year", year);
+            if(year % 100 == 0) {
+                if(year % 400 == 0) {
+                    System.out.println(year + " is a leap year.");
+                }
+                else {
+                    System.out.println(year + " is not a leap year");
+                }
+            }
+            else {
+                System.out.println(year + " is a leap year.");
+            }
         }
         else {
-            System.out.printf("%d is not a Leap year", year);
+            System.out.println(year + " is not a leap year.");
         }
     }
 }
